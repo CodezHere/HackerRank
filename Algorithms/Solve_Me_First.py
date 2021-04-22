a2b4244d380e1f397f2c2f1b7fab71de77be0bef
CodezHere <cm11_1@hotmail.com>
2021-04-21 23:45:53 -0400

    More Katas

--- Algorithms/Solve_Me_First.py
+++ Algorithms/Solve_Me_First.py
@@ -1,33 +1,33 @@
-# Complete the function solveMeFirst to compute the sum of two integers.
-#
-# Example
-# a = 7
-# b = 3
-# Return 10.
-#
-# Function Description
-# Complete the solveMeFirst function in the editor below.
+"""Complete the function solveMeFirst to compute the sum of two integers.
 
-# solveMeFirst has the following parameters:
+Example
+a = 7
+b = 3
+Return 10.
 
-# int a: the first value
-# int b: the second value
-# Returns
-# - int: the sum of a and b
+Function Description
+Complete the solveMeFirst function in the editor below.
 
-# Constraints
-# 1<=a, b<=1000
-#
-# Sample Input
-#
-# a = 2
-# b = 3
-# Sample Output
-#
-# 5
-# Explanation
-#
-# 2+3=5.
+solveMeFirst has the following parameters:
+
+int a: the first value
+int b: the second value
+Returns
+- int: the sum of a and b
+
+Constraints
+1<=a, b<=1000
+
+Sample Input
+
+a = 2
+b = 3
+Sample Output
+
+5
+Explanation
+2+3=5.
+"""
 
 def solveMeFirst(a,b):
 	# Hint: Type return a+b below
